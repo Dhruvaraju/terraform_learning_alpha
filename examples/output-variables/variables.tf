5ec7d9a1c3099ce08160bf26a3f27b65358004b1
Dhruva Raju <49dhruva@gmail.com>
2022-12-29 11:08:09 +0530

    refactor

--- examples/output-variables/variables.tf
+++ examples/output-variables/variables.tf
@@ -1,15 +1,15 @@
 variable "filename" {
-    default = "D:\\temp\\example.txt"
+  default = "D:\\temp\\example.txt"
 }
 
 variable "file_permission" {
-    default = "0700"
+  default = "0700"
 }
 
 variable "prefix" {
-    default = "Mrs"
+  default = "Mrs"
 }
 
 variable "seperator" {
-    default = "."
+  default = "."
 }
